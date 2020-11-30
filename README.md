e217f8954b91ecaf69fa2ae67992ffbca2b718b4
Wilber Anderson <wilber.anderson17@gmail.com>
2020-11-30 11:47:55 -0500

    Updated readme.

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ Automating archiving of my digital footprint.
 
 ## Dependencies 
 `youtube-dl`
+
 `gawk`
+
+
+## How to
+Some directories are hardcoded into files, specifically in `youtube-dl.conf` and `downloadqueue.sh`.
+
+0. Install dependencies.
+1. Run `updatelists.sh`
+2. Run `downloadqueue.sh`
--- downloadqueue.sh
+++ downloadqueue.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+echo "Reading queue:"
+cat queue.txt | while read id
+do
+	echo " Downloading $id"
+	# download using configuration file:
+	youtube-dl --config-location ~/Documents/Media/Archive/archiver/youtube-dl.conf "https://youtu.be/$id"
+	# add to master list:
+	echo "$id" >> master.txt
+done
